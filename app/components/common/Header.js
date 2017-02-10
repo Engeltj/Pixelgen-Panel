b53596815400311e60425d470d15ad1dc8ee26d2
Andrew Hartwig <andrew.hartwig1@gmail.com>
2017-02-10 13:41:25 -0500

    Added header component.

---
+++ app/components/common/Header.js
@@ -0,0 +1,39 @@
+import React, { Component, PropTypes } from 'react';
+import _ from 'lodash';
+import classNames from 'classnames';
+
+class Header extends Component {
+    render() {
+        let i = 0;
+        let { children } = this.props;
+        if (_.isObject(children)) {
+            children = [children];
+        }
+        let numberOfChildren = children.length;
+
+        return (
+            <div className="row wrapper border-bottom white-bg page-heading">
+                <div className="col-lg-10">
+                    <h2>{ this.props.title }</h2>
+                    {children.length && (
+                        <ol className="breadcrumb">
+                            {children.map(child => {
+                                const isLast = i+1 === numberOfChildren;
+                                return (
+                                    <li key={ i++ }>{ isLast ? <strong>{child}</strong> : child }</li>
+                                );
+                            })}
+                        </ol>
+                    )}
+                </div>
+                <div className="col-lg-2"></div>
+            </div>
+        );
+    }
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired
+};
+
+export default Header;
--- app/views/Users.js
+++ app/views/Users.js
@@ -1,42 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 import _ from 'lodash';
-
-const addOneToCount = function() {
-    return {
-        type: 'INCREMENT',
-        payload: 1
-    };
-}
-
-const mapStateToProps = function(state) {
-    return {
-        count: state.count
-    };
-}
-
-const mapDispatchToProps = function(dispatch) {
-    return {
-        increment() {
-            dispatch(addOneToCount());
-        }
-    }
-}
+import Header from '../components/common/Header';
 
 class Users extends Component {
 
     render() {
+        console.log('Rendering users..');
         return (
-            <div className="wrapper wrapper-content animated fadeInRight">
-                <div className="row">
-                    <div className="col-lg-12">
-                        <div className="text-center m-t-lg" style={{ userSelect: 'none' }}>
-                            <h1 onClick={ this.props.increment }>
-                                Count: { this.props.count }
-                            </h1>
-                            <small>
-                                It is an application skeleton for a typical web app. You can use it to quickly bootstrap your webapp projects.
-                            </small>
+            <div>
+                <Header title="Manage Users">
+                    <Link to="/users">Users</Link>
+                </Header>
+                <div className="wrapper wrapper-content">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="text-center m-t-lg" style={{ userSelect: 'none' }}>
+                                <h1>
+                                    Poop
+                                </h1>
+                                <small>
+                                    It is an application skeleton for a typical web app. You can use it to quickly bootstrap your webapp projects.
+                                </small>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -46,4 +33,4 @@ class Users extends Component {
 
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users)
+export default Users;
